b2620699bd2213754c7e14546723bfa3e0decfab
chaeyeonkim <codus3968@naver.com>
2024-05-27 14:17:35 +0900

    1.1.3 DAO test code using main() class

--- src/main/java/toby/spring/Main.java
+++ src/main/java/toby/spring/Main.java
@@ -1,6 +1,26 @@
 package toby.spring;
 
+import java.sql.SQLException;
+import toby.spring.object.dependecy.User;
+import toby.spring.object.dependecy.UserDao;
 
 public class Main {
-  public static void main(String[] args) {}
+  public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    UserDao dao = new UserDao();
+
+    User user = new User();
+    user.setId("whiteship");
+    user.setName("백기선");
+    user.setPassword("married");
+
+    dao.add(user);
+
+    System.out.println(user.getId() + "등록 성공");
+
+    User user2 = dao.get(user.getId());
+    System.out.println(user.getName());
+    System.out.println(user.getPassword());
+
+    System.out.println(user2.getId() + " 조회 성공");
+  }
 }
