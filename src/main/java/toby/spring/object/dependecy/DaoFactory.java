48ff48e76ffa74bffff1362ab6c0e9027a8d3667
chaeyeonkim <codus3968@naver.com>
2024-06-03 16:34:28 +0900

    1.5.1 오브젝트 팩토리를 이용한 스프링 IoC

--- src/main/java/toby/spring/object/dependecy/DaoFactory.java
+++ src/main/java/toby/spring/object/dependecy/DaoFactory.java
@@ -1,10 +1,17 @@
 package toby.spring.object.dependecy;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+
+  @Bean
   public UserDao userDao() {
     return new UserDao(connectionMaker());
   }
 
+  @Bean
   public ConnectionMaker connectionMaker() {
     // 분리해서 중복을 제거한 ConnectionMaker 타입 오브젝트 생성 코드
     return new DConnectionMaker();
--- src/main/java/toby/spring/object/dependecy/UserDaoTest.java
+++ src/main/java/toby/spring/object/dependecy/UserDaoTest.java
@@ -1,11 +1,14 @@
 package toby.spring.object.dependecy;
 
 import java.sql.SQLException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class UserDaoTest {
   public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    UserDao dao = context.getBean("userDao", UserDao.class);
 
-    UserDao dao = new DaoFactory().userDao();
     User user = new User();
 
     user.setId("whiteship");
