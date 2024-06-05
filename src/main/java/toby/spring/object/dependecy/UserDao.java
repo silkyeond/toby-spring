fb1068d7f4ea26bd1a3b66aa363fbbc6e1f20ce5
chaeyeonkim <codus3968@naver.com>
2024-06-05 15:27:38 +0900

    2.3.3 포괄적인 테스트

--- src/main/java/toby/spring/object/dependecy/UserDao.java
+++ src/main/java/toby/spring/object/dependecy/UserDao.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 public class UserDao {
   private DataSource dataSource;
@@ -37,7 +38,7 @@ public class UserDao {
   //    this.connectionMaker = context.getBean("connectionMaker", ConnectionMaker.class);
   //  }
 
-  public void add(User user) throws ClassNotFoundException, SQLException {
+  public void add(User user) throws SQLException {
     //    Connection c = connectionMaker.makeConnection();
     Connection c = dataSource.getConnection();
     PreparedStatement ps =
@@ -52,22 +53,55 @@ public class UserDao {
     c.close();
   }
 
-  public User get(String id) throws ClassNotFoundException, SQLException {
-//    Connection c = connectionMaker.makeConnection();
+  public User get(String id) throws SQLException {
+    //    Connection c = connectionMaker.makeConnection();
     Connection c = dataSource.getConnection();
     PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
     ps.setString(1, id);
 
     ResultSet rs = ps.executeQuery();
-    rs.next();
-    User user = new User();
-    user.setId(rs.getString("id"));
-    user.setName(rs.getString("name"));
-    user.setPassword(rs.getString("password"));
+    User user = null;
+
+    if (rs.next()) {
+      user = new User();
+      user.setId(rs.getString("id"));
+      user.setName(rs.getString("name"));
+      user.setPassword(rs.getString("password"));
+    }
 
     rs.close();
     ps.close();
     c.close();
+
+    if (user == null) {
+      throw new EmptyResultDataAccessException(1);
+    }
+
     return user;
+  }
+
+  public void deleteAll() throws SQLException {
+    Connection c = dataSource.getConnection();
+    PreparedStatement ps = c.prepareStatement("delete from users");
+    ps.executeUpdate();
+
+    ps.close();
+    c.close();
+  }
+
+  public int getCount() throws SQLException {
+    Connection c = dataSource.getConnection();
+
+    PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+    ResultSet rs = ps.executeQuery();
+    rs.next();
+    int count = rs.getInt(1);
+
+    rs.close();
+    ps.close();
+    c.close();
+
+    return count;
   }
 }
---
+++ src/test/java/UserDaoTest.java
@@ -0,0 +1,46 @@
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import toby.spring.object.dependecy.User;
+import toby.spring.object.dependecy.UserDao;
+
+public class UserDaoTest {
+  @Test
+  public void addAndGet() throws SQLException {
+    ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+    UserDao dao = context.getBean("userDao", UserDao.class);
+
+    User user1 = new User("gyumee", "박상철", "springno1");
+    User user2 = new User("leegw700", "이길원", "springno2");
+
+    dao.deleteAll();
+    assertThat(dao.getCount()).isEqualTo(0);
+
+    dao.add(user1);
+    dao.add(user2);
+    assertThat(dao.getCount()).isEqualTo(2);
+
+    User userGet1 = dao.get(user1.getId());
+    assertThat(userGet1.getName()).isEqualTo(user1.getName());
+    assertThat(userGet1.getPassword()).isEqualTo(user1.getPassword());
+
+    User userGet2 = dao.get(user2.getId());
+    assertThat(userGet2.getName()).isEqualTo(user2.getName());
+    assertThat(userGet2.getPassword()).isEqualTo(user2.getPassword());
+  }
+
+  @Test
+  public void getUserFailure() throws SQLException {
+    ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+    UserDao dao = context.getBean("userDao", UserDao.class);
+
+    dao.deleteAll();
+    assertThat(dao.getCount()).isEqualTo(0);
+    assertThrows(EmptyResultDataAccessException.class, () -> dao.get("unknown_id"));
+  }
+}
