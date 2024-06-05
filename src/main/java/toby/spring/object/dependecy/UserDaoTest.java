d8abd26248f5eb4ac81094ba91622780c7114d97
chaeyeonkim <codus3968@naver.com>
2024-06-05 10:21:31 +0900

    2.2.1 테스트 검증의 자동화

--- src/main/java/toby/spring/object/dependecy/UserDaoTest.java
+++ src/main/java/toby/spring/object/dependecy/UserDaoTest.java
@@ -13,7 +13,6 @@ public class UserDaoTest {
     ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
     UserDao dao = context.getBean("userDao", UserDao.class);
-    //    UserDao dao = new UserDao();
     User user = new User();
 
     user.setId("whiteship");
@@ -22,6 +21,14 @@ public class UserDaoTest {
 
     dao.add(user);
 
-    System.out.println(user.getId() + " 등록 성공");
+    User user2 = dao.get(user.getId());
+
+    if (!user.getName().equals(user2.getName())) {
+      System.out.println("테스트 실패 (name)");
+    } else if (!user.getPassword().equals(user2.getPassword())) {
+      System.out.println("테스트 실패 (password)");
+    } else {
+      System.out.println("조회 테스트 성공");
+    }
   }
 }
