08912b4b12add8d451db475b9eeeafd353027cfd
chaeyeonkim <codus3968@ust21.co.kr>
2024-06-26 13:44:49 +0900

    refactoring

--- src/main/java/toby/spring/object/dependecy/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package toby.spring.object.dependecy;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-  public Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
--- src/main/java/toby/spring/object/dependecy/StatementStrategy.java
+++
@@ -1,9 +0,0 @@
-package toby.spring.object.dependecy;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface StatementStrategy {
-  PreparedStatement makePreparedStatement(Connection c) throws SQLException;
-}
