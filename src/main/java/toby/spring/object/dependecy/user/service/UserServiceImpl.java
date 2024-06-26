6928ca762712f2f419c8da6c66fb52cb4a7fd651
chaeyeonkim <codus3968@ust21.co.kr>
2024-06-26 16:35:54 +0900

    6.1.2 DI를 이용한 클래스의 분리

---
+++ src/main/java/toby/spring/object/dependecy/user/service/UserServiceImpl.java
@@ -0,0 +1,69 @@
+package toby.spring.object.dependecy.user.service;
+
+import java.util.List;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import toby.spring.object.dependecy.dao.UserDao;
+import toby.spring.object.dependecy.user.domain.Level;
+import toby.spring.object.dependecy.user.domain.User;
+
+public class UserServiceImpl implements UserService {
+  UserDao userDao;
+
+  public void setUserDao(UserDao userDao) {
+    this.userDao = userDao;
+  }
+
+  private MailSender mailSender;
+
+  public void setMailSender(MailSender mailSender) {
+    this.mailSender = mailSender;
+  }
+
+  public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+  public static final int MIN_RECCOMEND_ROR_GOLD = 30;
+
+  public void upgradeLevels() {
+    List<User> users = userDao.getAll();
+    for (User user : users) {
+      if (canUpgradeLevel(user)) {
+        upgradeLevel(user);
+      }
+    }
+  }
+
+  public void add(User user) {
+    if (user.getLevel() == null) user.setLevel(Level.BASIC);
+    userDao.add(user);
+  }
+
+  private boolean canUpgradeLevel(User user) {
+    Level currentLevel = user.getLevel();
+    switch (currentLevel) {
+      case BASIC:
+        return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
+      case SILVER:
+        return (user.getRecommend() >= MIN_RECCOMEND_ROR_GOLD);
+      case GOLD:
+        return false;
+      default:
+        throw new IllegalArgumentException("Unknown Level : " + currentLevel);
+    }
+  }
+
+  protected void upgradeLevel(User user) {
+    user.upgradeLevel();
+    userDao.update(user);
+    sendUpgradeEMail(user);
+  }
+
+  private void sendUpgradeEMail(User user) {
+    SimpleMailMessage mailMessage = new SimpleMailMessage();
+    mailMessage.setTo(user.getMail());
+    mailMessage.setFrom("useradmin@ksug.org");
+    mailMessage.setSubject("Upgrade 안내");
+    mailMessage.setText("사용자님의 등급이 " + user.getLevel().name());
+
+    this.mailSender.send(mailMessage);
+  }
+}
