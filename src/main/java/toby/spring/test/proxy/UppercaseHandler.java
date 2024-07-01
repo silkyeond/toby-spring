4f8ac93f3b0c22b30c62dd303ad595601c828c22
chaeyeonkim <codus3968@ust21.co.kr>
2024-07-01 20:37:48 +0900

    6.3.2 다이내믹 프록시 - 다이내믹 프록시 확장

--- src/main/java/toby/spring/test/proxy/UppercaseHandler.java
+++ src/main/java/toby/spring/test/proxy/UppercaseHandler.java
@@ -4,15 +4,19 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
-  Hello target;
+  Object target;
 
-  public UppercaseHandler(Hello target) {
+  public UppercaseHandler(Object target) {
     this.target = target;
   }
 
   @Override
   public Object invoke(Object o, Method method, Object[] args) throws Throwable {
-    String ret = (String) method.invoke(target, args); // 타깃으로 위임. 인터페이스의 메소드 호출에 모두 적용
-    return ret.toUpperCase(); // 부가기능 제공
+    Object ret = method.invoke(target, args);
+    if (ret instanceof String) {
+      return ((String) ret).toUpperCase();
+    } else {
+      return ret;
+    }
   }
 }
