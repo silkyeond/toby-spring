c7a26326f44856d9707a93ee98a502ec8338d3a5
chaeyeonkim <codus3968@ust21.co.kr>
2024-07-15 18:28:26 +0900

    6.7.2 트랜잭션 애노테이션 적용

--- src/main/java/toby/spring/object/dependecy/user/service/UserService.java
+++ src/main/java/toby/spring/object/dependecy/user/service/UserService.java
@@ -1,13 +1,18 @@
 package toby.spring.object.dependecy.user.service;
 
 import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
 import toby.spring.object.dependecy.user.domain.User;
 
+@Transactional
 public interface UserService {
   void add(User user);
 
+  @Transactional(readOnly=true)
   User get(String id);
 
+  @Transactional(readOnly=true)
   List<User> getAll();
 
   void deleteAll();
