45bd684278efc63807255da87244fabceeb24669
chaeyeonkim <codus3968@ust21.co.kr>
2024-07-22 20:47:55 +0900

    7.2.2 XML 파일을 이용하는 SQL 서비스

---
+++ src/main/java/toby/spring/object/dependecy/user/sqlservice/XmlSqlService.java
@@ -0,0 +1,40 @@
+package toby.spring.object.dependecy.user.sqlservice;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import toby.spring.object.dependecy.user.sqlservice.jaxb.SqlType;
+import toby.spring.object.dependecy.user.sqlservice.jaxb.Sqlmap;
+
+public class XmlSqlService implements SqlService {
+  private Map<String, String> sqlMap = new HashMap<>();
+
+  public XmlSqlService() {
+    String contextPath = Sqlmap.class.getPackage().getName();
+    try {
+      JAXBContext context = JAXBContext.newInstance(contextPath);
+      Unmarshaller unmarshaller = context.createUnmarshaller();
+      InputStream is = this.getClass().getClassLoader().getResourceAsStream("/sqlmap.xml");
+      Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+
+      for (SqlType sql : sqlmap.getSql()) {
+        sqlMap.put(sql.getKey(), sql.getValue());
+      }
+    } catch (JAXBException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public String getSql(String key) throws SqlRetrievalFailureException {
+    String sql = sqlMap.get(key);
+    if (sql == null) {
+      throw new SqlRetrievalFailureException(key + " not found");
+    } else {
+      return sql;
+    }
+  }
+}
