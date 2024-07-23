135a0bbbdacea0b40a88ec2f0fff76a18cc10ed1
chaeyeonkim <codus3968@ust21.co.kr>
2024-07-23 14:39:50 +0900

    7.2.3 빈의 초기화 작업

--- src/main/java/toby/spring/object/dependecy/user/sqlservice/XmlSqlService.java
+++ src/main/java/toby/spring/object/dependecy/user/sqlservice/XmlSqlService.java
@@ -3,6 +3,7 @@ package toby.spring.object.dependecy.user.sqlservice;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -11,13 +12,19 @@ import toby.spring.object.dependecy.user.sqlservice.jaxb.Sqlmap;
 
 public class XmlSqlService implements SqlService {
   private Map<String, String> sqlMap = new HashMap<>();
+  private String sqlmapFile;
 
-  public XmlSqlService() {
+  public void setSqlmapFile(String sqlmapFile) {
+    this.sqlmapFile = sqlmapFile;
+  }
+
+  @PostConstruct
+  public void loadSql() {
     String contextPath = Sqlmap.class.getPackage().getName();
     try {
       JAXBContext context = JAXBContext.newInstance(contextPath);
       Unmarshaller unmarshaller = context.createUnmarshaller();
-      InputStream is = this.getClass().getClassLoader().getResourceAsStream("sqlmap.xml");
+      InputStream is = this.getClass().getClassLoader().getResourceAsStream(this.sqlmapFile);
       Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
       for (SqlType sql : sqlmap.getSql()) {
