4c44211636ed7709a635feed00348acc76fe9cf3
chaeyeonkim <codus3968@ust21.co.kr>
2024-07-25 10:45:26 +0900

    7.3.2 OXM 서비스 추상화 적용 - 위임을 이용한 BaseSqlService의 재사용

--- src/main/java/toby/spring/object/dependecy/user/sqlservice/OxmSqlService.java
+++ src/main/java/toby/spring/object/dependecy/user/sqlservice/OxmSqlService.java
@@ -11,6 +11,7 @@ import toby.spring.object.dependecy.user.sqlservice.jaxb.Sqlmap;
 public class OxmSqlService implements SqlService {
   private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
   private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
+  private final BaseSqlService baseSqlService = new BaseSqlService();
 
   public void setSqlRegistry(SqlRegistry sqlRegistry) {
     this.sqlRegistry = sqlRegistry;
@@ -26,16 +27,15 @@ public class OxmSqlService implements SqlService {
 
   @Override
   public String getSql(String key) throws SqlRetrievalFailureException {
-    try {
-      return this.sqlRegistry.findSql(key);
-    } catch (SqlNotFoundException e) {
-      throw new SqlRetrievalFailureException(e + " SQL Not Found");
-    }
+    return this.baseSqlService.getSql(key);
   }
 
   @PostConstruct
   public void loadSql() {
-    this.oxmSqlReader.read(this.sqlRegistry);
+    this.baseSqlService.setSqlReader(this.oxmSqlReader);
+    this.baseSqlService.setSqlRegistry(this.sqlRegistry);
+
+    this.baseSqlService.loadSql();
   }
 
   private class OxmSqlReader implements SqlReader {
