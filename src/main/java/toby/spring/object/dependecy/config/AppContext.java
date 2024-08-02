eec5bd4a4f9be8e6353acc9528ec029185f865af
chaeyeonkim <codus3968@ust21.co.kr>
2024-08-02 10:18:08 +0900

    7.6.5 프로퍼티 소스

--- src/main/java/toby/spring/object/dependecy/config/AppContext.java
+++ src/main/java/toby/spring/object/dependecy/config/AppContext.java
@@ -2,14 +2,17 @@ package toby.spring.object.dependecy.config;
 
 import static toby.spring.object.dependecy.config.AppContext.*;
 
-import com.mysql.cj.jdbc.Driver;
+import java.sql.Driver;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -22,18 +25,30 @@ import toby.spring.object.dependecy.dao.UserDao;
 @EnableTransactionManagement
 @ComponentScan(basePackages = "toby.spring.object.dependecy")
 @Import({SqlServiceContext.class, ProductionAppContext.class})
+@PropertySource("/database.properties")
 public class AppContext {
 
   @Autowired UserDao userDao;
 
+  @Value("${db.driverClass}")
+  Class<? extends Driver> dbDriverClass;
+
+  @Value("${db.url}")
+  String url;
+
+  @Value("${db.username}")
+  String username;
+
+  @Value("${db.password}")
+  String password;
+
   @Bean
   public DataSource dataSource() {
     SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-
-    dataSource.setDriverClass(Driver.class);
-    dataSource.setUrl("jdbc:mysql://localhost/springbook?characterEncoding=UTF-8");
-    dataSource.setUsername("spring");
-    dataSource.setPassword("book");
+    dataSource.setDriverClass(this.dbDriverClass);
+    dataSource.setUrl(this.url);
+    dataSource.setUsername(this.username);
+    dataSource.setPassword(this.password);
 
     return dataSource;
   }
@@ -61,5 +76,10 @@ public class AppContext {
       mailSender.setHost("localhost");
       return mailSender;
     }
+  }
+
+  @Bean
+  public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+    return new PropertySourcesPlaceholderConfigurer();
   }
 }
